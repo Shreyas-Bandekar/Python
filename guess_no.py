de6e89f3a81e6eed1a453684eaa015f4e848400c
Shreyas-Bandekar <shreyasbandekar01@gmail.com>
2025-05-11 11:05:24 +0530

    complete basic version of Guess the Number game

--- guess_no.py
+++ guess_no.py
@@ -1,7 +1,11 @@
-import random
-n = random.randint(1,100)
+# Guess the number game
+
+import random # For generating a random number
+
+n = random.randint(1,100)  # Random number between 1 and 100
 a = -1
-guesses = 0
+guesses = 0   # Count of user attempts
+
 while(a!=n):
     a = int(input("Enter a number: "))
     guesses += 1
