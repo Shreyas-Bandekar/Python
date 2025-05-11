151a66013d95621133a60a45ae0293d5a0c83669
Shreyas-Bandekar <shreyasbandekar01@gmail.com>
2025-05-11 10:59:35 +0530

    Logic for showing no of attempts taken by user to complete the game

--- guess_no.py
+++ guess_no.py
@@ -1,16 +1,21 @@
 import random
 n = random.randint(1,100)
 a = -1
-
+guesses = 0
 while(a!=n):
     a = int(input("Enter a number: "))
+    guesses += 1
     if(a>n):
         print(f"Guess the number which is lesser than {a}")
     elif(a<n):
          print(f"Guess the number have value is greater than {a}")
     elif(a==n):
         print("Congratulations your guess is correct!!")
+        print(f"You took {guesses} attempts")
     else:
         print("Something is wrong")
+
+
+
 
 
